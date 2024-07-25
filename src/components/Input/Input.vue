aed5aaed4aaf5b03653262f039982ee9fbd6bc3e
v-chenx83 <1614819989@qq.com>
2024-07-25 21:42:38 +0800

    Input组件支持密码模式

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,125 +1,153 @@
 <script setup lang="ts">
-import type { InputProps, InputEmits } from './types'
-import { ref, watch, computed } from 'vue'
-import Icon from '../Icon/Icon.vue'
+import type { InputProps, InputEmits } from "./types";
+import { ref, watch, computed } from "vue";
+import Icon from "../Icon/Icon.vue";
 
 // 定义组件属性
 defineOptions({
-  name: 'xxInput',
-})
+  name: "xxInput",
+});
 
-const isFocus = ref(false)
+const isFocus = ref(false);
+const passwordVisible = ref(false);
 
 // 接收父组件值
-const props = withDefaults(defineProps<InputProps>(), {type: 'text'})
+const props = withDefaults(defineProps<InputProps>(), { type: "text" });
 
 // 注册自定义事件
-const emits = defineEmits<InputEmits>()
+const emits = defineEmits<InputEmits>();
 
-const innerValue = ref(props.modelValue)
+const innerValue = ref(props.modelValue);
 
 // 更新输入值
 const handleInput = () => {
-  emits('update:modelValue', innerValue.value)
-}
+  emits("update:modelValue", innerValue.value);
+};
 
 // 监听modelValue值变化
-watch(() => props.modelValue, (newValue) => {
-  innerValue.value = newValue
-})
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    innerValue.value = newValue;
+  }
+);
 
 // 判断什么时候显示清空按钮
-const showClear = computed(() => 
-  props.clearable && 
-  !props.disabled && 
-  !!innerValue.value && 
-  isFocus.value &&
-  props.type !== 'textarea'
-)
+const showClear = computed(
+  () =>
+    props.clearable &&
+    !props.disabled &&
+    !!innerValue.value &&
+    isFocus.value &&
+    props.type !== "textarea"
+);
 
 // 处理focus值
 const handleFocus = () => {
-  isFocus.value = true
-}
+  isFocus.value = true;
+};
 const handleBlur = () => {
-  isFocus.value = false
-}
+  isFocus.value = false;
+};
 
 // 处理清空按钮点击
 const clear = () => {
-  innerValue.value = ''
-  emits('update:modelValue', '')
-}
+  innerValue.value = "";
+  emits("update:modelValue", "");
+};
 
+// 处理密码显示
+const showPasswordArea = computed(
+  () => props.showPassword && !props.disabled && !!innerValue.value
+);
+
+// 处理密码显示切换
+const togglePasswordVisible = () => {
+  passwordVisible.value = !passwordVisible.value;
+};
 </script>
 
 <template>
+  <div
+    class="xx-input"
+    :class="{
+      [`xx-input--${type}`]: type,
+      [`xx-input--${size}`]: size,
+      'is-disabled': disabled,
+      'is-prepend': $slots.prepend,
+      'is-append': $slots.append,
+      'is-prefix': $slots.prefix,
+      'is-suffix': $slots.suffix,
+      'is-focus': isFocus,
+    }"
+  >
+    <!-- input -->
+    <template v-if="type !== 'textarea'">
+      <!-- prepend slot -->
+      <div v-if="$slots.prepend" class="xx-input__prepend">
+        <slot name="prepend" />
+      </div>
 
-<div
-  class="xx-input"
-  :class="{
-    [`xx-input--${type}`]: type,
-    [`xx-input--${size}`]: size,
-    'is-disabled': disabled,
-    'is-prepend': $slots.prepend,
-    'is-append': $slots.append,
-    'is-prefix': $slots.prefix,
-    'is-suffix': $slots.suffix,
-    'is-focus': isFocus,
-  }"
->
-  <!-- input -->
-  <template v-if="type !== 'textarea'">
-    <!-- prepend slot -->
-    <div v-if="$slots.prepend" class="xx-input__prepend">
-      <slot name="prepend" />
-    </div>
-
-    <!-- input wrapper -->
-    <div class="xx-input__wrapper">
-      <!-- prefix slot -->
-      <span v-if="$slots.prefix" class="xx-input__prefix">
+      <!-- input wrapper -->
+      <div class="xx-input__wrapper">
+        <!-- prefix slot -->
+        <span v-if="$slots.prefix" class="xx-input__prefix">
           <slot name="prefix" />
-      </span>
+        </span>
 
-      <input
-        class="xx-input__inner"
-        :type="type"
+        <input
+          class="xx-input__inner"
+          :type="showPassword ? (passwordVisible ? 'text' : 'password') : type"
+          :disabled="disabled"
+          v-model="innerValue"
+          @input="handleInput"
+          @focus="handleFocus"
+          @blur="handleBlur"
+        />
+
+        <!-- suffix slot -->
+        <span
+          v-if="$slots.suffix || showClear || showPasswordArea"
+          class="xx-input__suffix"
+        >
+          <slot name="suffix" />
+          <!-- clear button -->
+          <Icon
+            icon="circle-xmark"
+            v-if="showClear"
+            class="xx-input__clear"
+            @click="clear"
+          />
+          <!-- password button -->
+          <Icon
+            icon="eye"
+            v-if="showPasswordArea && passwordVisible"
+            class="xx-input__password"
+            @click="togglePasswordVisible"
+          />
+          <Icon
+            icon="eye-slash"
+            v-if="showPasswordArea && !passwordVisible"
+            class="xx-input__password"
+            @click="togglePasswordVisible"
+          />
+        </span>
+      </div>
+
+      <!-- append slot -->
+      <div v-if="$slots.append" class="xx-input__append">
+        <slot name="append" />
+      </div>
+    </template>
+
+    <!-- textarea -->
+    <template v-else>
+      <textarea
+        class="xx-input__wrapper"
         :disabled="disabled"
         v-model="innerValue"
         @input="handleInput"
-        @focus="handleFocus"
-        @blur="handleBlur"
       />
-
-      <!-- suffix slot -->
-      <span v-if="$slots.suffix || showClear" class="xx-input__suffix">
-        <slot name="suffix" />
-        <Icon 
-          icon="circle-xmark"
-          v-if="showClear"
-          class="xx-input__clear"
-          @click="clear"
-        />
-      </span>
-    </div>
-
-    <!-- append slot -->
-    <div v-if="$slots.append" class="xx-input__append">
-      <slot name="append" />
-    </div>
-
-  </template>
-
-  <!-- textarea -->
-  <template v-else>
-    <textarea
-      class="xx-input__wrapper"
-      :disabled="disabled"
-      v-model="innerValue"
-      @input="handleInput"
-    />
-  </template>
-</div>
-
+    </template>
+  </div>
 </template>
--- src/components/Input/input.test.ts
+++ src/components/Input/input.test.ts
@@ -1,85 +1,110 @@
-import { describe, expect, it } from 'vitest'
-import { mount } from '@vue/test-utils'
-import Input from './Input.vue'
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import Input from "./Input.vue";
 
-describe('Input', () => {
-  it('基本展示', () => {
+describe("Input", () => {
+  it("基本展示", () => {
     // 针对动态 class，查看 classes 是否正确
     const wrapper = mount(Input, {
       props: {
-        size: 'small',
-        type: 'text',
-        modelValue: '',
+        size: "small",
+        type: "text",
+        modelValue: "",
       },
       slots: {
-        prepend: 'prepend',
-        prefix: 'prefix'
-      }
-    })
-    expect(wrapper.classes()).toContain('xx-input--small')
-    expect(wrapper.classes()).toContain('is-prepend')
+        prepend: "prepend",
+        prefix: "prefix",
+      },
+    });
+    expect(wrapper.classes()).toContain("xx-input--small");
+    expect(wrapper.classes()).toContain("is-prepend");
 
     // 针对 v-if 是否渲染正确的标签以及内容
-    expect(wrapper.find('input').exists()).toBeTruthy()
-    expect(wrapper.get('input').attributes('type')).toBe('text')
+    expect(wrapper.find("input").exists()).toBeTruthy();
+    expect(wrapper.get("input").attributes("type")).toBe("text");
 
     // 针对 slots，是否渲染对应的 slots 内容
-    expect(wrapper.find('.xx-input__prepend').exists()).toBeTruthy()
-    expect(wrapper.get('.xx-input__prepend').text()).toBe('prepend')
-    expect(wrapper.find('.xx-input__prefix').exists()).toBeTruthy()
-    expect(wrapper.get('.xx-input__prefix').text()).toBe('prefix')
-
+    expect(wrapper.find(".xx-input__prepend").exists()).toBeTruthy();
+    expect(wrapper.get(".xx-input__prepend").text()).toBe("prepend");
+    expect(wrapper.find(".xx-input__prefix").exists()).toBeTruthy();
+    expect(wrapper.get(".xx-input__prefix").text()).toBe("prefix");
 
     // 测试textarea
     const wrapper2 = mount(Input, {
       props: {
-        type: 'textarea',
-        modelValue: ''
-      }
-    })
-    expect(wrapper2.find('textarea').exists()).toBeTruthy()
-  })
+        type: "textarea",
+        modelValue: "",
+      },
+    });
+    expect(wrapper2.find("textarea").exists()).toBeTruthy();
+  });
 
-  it('支持v-model', async() => {
+  it("支持v-model", async () => {
     const wrapper = mount(Input, {
       props: {
-        modelValue: 'test',
-        'onUpdate:modelValue': (e: any) => wrapper.setProps({ modelValue: e })
-      }
-    })
+        modelValue: "test",
+        "onUpdate:modelValue": (e: any) => wrapper.setProps({ modelValue: e }),
+      },
+    });
     // 测试初始值
-    const input = wrapper.get('input')
-    expect(input.element.value).toBe('test')
+    const input = wrapper.get("input");
+    expect(input.element.value).toBe("test");
 
     // 测试更新值
     // 注意 setValue 是组合事件会触发 input 以及 change
-    await input.setValue('update')
-    expect(wrapper.props('modelValue')).toBe('update')
-    expect(input.element.value).toBe('update')
+    await input.setValue("update");
+    expect(wrapper.props("modelValue")).toBe("update");
+    expect(input.element.value).toBe("update");
 
     // v-model 的异步更新
-    await wrapper.setProps({ modelValue: 'prop update' })
-    expect(input.element.value).toBe('prop update')
-  })
-  it.only('支持clearable', async() => {
+    await wrapper.setProps({ modelValue: "prop update" });
+    expect(input.element.value).toBe("prop update");
+  });
+  it("支持clearable", async () => {
     const wrapper = mount(Input, {
       props: {
-        modelValue: 'test',
+        modelValue: "test",
         clearable: true,
-        type: 'text',
+        type: "text",
       },
       global: {
-        stubs: ['Icon']
-      }
-    })
+        stubs: ["Icon"],
+      },
+    });
     // 不出现对应的 Icon 区域, 因为未focus
-    expect(wrapper.find('.xx-input__clear').exists()).toBeFalsy()
-    const input = wrapper.get('input')
-    await input.trigger('focus')
+    expect(wrapper.find(".xx-input__clear").exists()).toBeFalsy();
+    const input = wrapper.get("input");
+    await input.trigger("focus");
     // 出现对应的 Icon 区域, 因为focus
-    expect(wrapper.find('.xx-input__clear').exists()).toBeTruthy()
+    expect(wrapper.find(".xx-input__clear").exists()).toBeTruthy();
     // 点击 Icon 区域, 输入框清空
-    await wrapper.find('.xx-input__clear').trigger('click')
-    expect(input.element.value).toBe('')
-  })
-})
+    await wrapper.find(".xx-input__clear").trigger("click");
+    expect(input.element.value).toBe("");
+  });
+
+  it("支持切换密码显示", async () => {
+    const wrapper = mount(Input, {
+      props: {
+        modelValue: "",
+        showPassword: true,
+        type: "text",
+      },
+      global: {
+        stubs: ["Icon"],
+      },
+    });
+    // 不出现对应的 Icon 区域, 因为当前值为空
+    expect(wrapper.find('.xx-input__password').exists()).toBeFalsy()
+    const input = wrapper.get('input')
+    expect(input.element.type).toBe('password')
+    //  出现 Icon 区域，并且 Icon 为特定的图标
+    await input.setValue('123')
+    const eyeIcon = wrapper.find('.xx-input__password')
+    expect(eyeIcon.exists()).toBeTruthy()
+    expect(eyeIcon.attributes('icon')).toBe('eye-slash')
+    // 点击值变会切换input 类型，并且图标的 Icon 会切换
+    await eyeIcon.trigger('click')
+    expect(input.element.type).toBe('text')
+    expect(wrapper.find('.xx-input__password').attributes('icon')).toBe('eye')
+  });
+});
